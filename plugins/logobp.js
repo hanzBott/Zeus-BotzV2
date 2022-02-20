e8ba56e813fb72bfac027b718ec97b048e272370
hanzBott <95454241+hanzBott@users.noreply.github.com>
2022-02-21 06:09:32 +0700

    Update logobp.js

--- plugins/logobp.js
+++ plugins/logobp.js
@@ -2,9 +2,9 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args) throw 'Masukkan Parameter'
-  m.reply('_Proses..._')
+  m.reply('_Proses...,proses nya lama?,berarti lu kaga donasi_')
   let res = `https://api.zeks.xyz/api/logobp?text=${response}&apikey=apivinz`
-  conn.sendFile(m.chat, res, 'logobp.jpg', `Nih kak`, m, false)
+  conn.sendFile(m.chat, res, 'logobp.jpg', `Done`, m, false)
 }
 handler.help = ['logobp'].map(v => v + ' <teks>')
 handler.tags = ['sticker']
